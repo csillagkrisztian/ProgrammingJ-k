ace2cba36cd0546beb68ac630b6cc427119d7829
csillagkrisztian <krisztian.csillag93@gmail.com>
2020-09-03 10:14:37 +0200

    initial npm roundup and a new Homepage page added

---
+++ src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function Homepage() {
+  return (
+    <Container>
+      <Row>
+        <h1> Welcome to _jöök</h1>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
+}
