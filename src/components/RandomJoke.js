2ec01c3cca6e5fb288f03d3e2ec00e7cfca3cb09
csillagkrisztian <krisztian.csillag93@gmail.com>
2020-09-03 11:06:36 +0200

    added a Random joke component that gets a random joke and displayed the data on the homepage

---
+++ src/components/RandomJoke.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function RandomJoke() {
+  const initalState = { setup: "", punchline: "" };
+  const [joke, setJoke] = useState(initalState);
+  const [punchline, setPunchline] = useState("");
+
+  console.log(punchline, joke.punchline);
+
+  useEffect(() => {
+    async function getRandomJoke() {
+      try {
+        const response = await axios.get(
+          `https://official-joke-api.appspot.com/jokes/programming/random`
+        );
+        setJoke(response.data[0]);
+      } catch (error) {
+        setJoke({
+          setup: "Now that's not funny... We couldn't get the joke... ",
+        });
+      }
+    }
+    getRandomJoke();
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setPunchline(joke.punchline);
+    }, 4000);
+    return () => clearTimeout(timer);
+  }, [joke]);
+
+  return !joke.setup ? (
+    <div>Loading...</div>
+  ) : (
+    <div>
+      <p>{joke.setup}</p>
+      {!punchline ? <p>Wait for it...</p> : punchline}
+    </div>
+  );
+}
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import RandomJoke from "../components/RandomJoke";
+import "./Homepage.css";
 
 export default function Homepage() {
   return (
-    <Container>
-      <Row>
-        <h1> Welcome to _jöök</h1>
-        <Col></Col>
+    <Container fluid className="homepage-container">
+      <Row className="homepage-title-row">
+        <h1 className="homepage-title"> Welcome to Programming Jöök</h1>
+      </Row>
+      <Row className="homepage-joke-row">
+        <Col className="homepage-joke-col">
+          <h2 className="homepage-joke-title">Here is a Jöök for you!</h2>
+          <RandomJoke />
+        </Col>
+        <Col className="homepage-joke-gif">
+          <img></img>
+        </Col>
       </Row>
     </Container>
   );
